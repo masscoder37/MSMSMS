0b2523c53ba34f4109c0ec7763b556f64a13285b
Michael Stadlmeier <michael.stadlmeier@gmail.com>
2024-05-28 21:58:16 +0200

    first commit

---
+++ MSMSMS/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Michael S. Mass Spec Methodology Suite - MS³
+ * tools for peptide-centric MS analysis
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
